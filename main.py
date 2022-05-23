758a30502d7b9f4b20ae898625bc70a18799b79a
Roman0104 <roman0104@rambler.ru>
2022-05-23 21:00:42 +0300

    download book covers

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import requests
+import os
 from pathlib import Path
 from bs4 import BeautifulSoup
 from typing import Optional
 from pathvalidate import sanitize_filename
+from urllib.parse import urljoin, urlsplit
 
 
 def check_for_redirect(response):
@@ -29,17 +31,38 @@ def download_txt(url: str, filename: str, folder: str = "books/") -> Optional[st
 
     Path(folder).mkdir(parents=True, exist_ok=True)
 
-    path_filename = f"{folder}{sanitize_filename(filename)}.txt"
+    path_filename = os.path.join(folder, f"{sanitize_filename(filename)}.txt")
 
     with open(path_filename, "w", encoding="utf-8") as file:
         file.write(response.text)
     return path_filename
 
 
+def download_img(url: str, folder: str = "images/") -> Optional[str]:
+    """Функция для скачивания картинок файлов.
+    Args:
+        url (str): Cсылка на картинку, которую хочется скачать.
+        folder (str): Папка, куда сохранять.
+    Returns:
+        str: Путь до файла, куда сохранён текст.
+    """
+    response = requests.get(url)
+    response.raise_for_status()
+
+    Path(folder).mkdir(parents=True, exist_ok=True)
+    filename = urlsplit(url, scheme='', allow_fragments=True).path.split("/")[-1]
+
+    path_filename = os.path.join(folder, filename)
+
+    with open(path_filename, "wb") as file:
+        file.write(response.content)
+    return path_filename
+
+
 def main(url: str) -> None:
 
     for index in range(1, 11):
-        url_page = url + "/b" + str(index) + "/"
+        url_page = f"{url}/b{index}/"
         response = requests.get(url_page)
         response.raise_for_status()
 
@@ -54,10 +77,14 @@ def main(url: str) -> None:
 
         title = title_text.split("\xa0 :: \xa0")[0].strip()
 
-        url_txt = url + "txt.php?id=" + str(index)
-        filename = f"{index}. {title}"
+        link_img = soup.find("div", class_="bookimage").find("img")["src"]
+        url_img = urljoin(url, link_img)
 
-        download_txt(url_txt, filename)
+        url_txt = f"{url}txt.php?id={index}"
+        filename_text = f"{index}. {title}"
+
+        download_txt(url_txt, filename_text)
+        download_img(url_img)
 
 
 if __name__ == "__main__":
