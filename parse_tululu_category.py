a1fc87ab0b07fe7848dde7d03e431b1b3c66b0ec
Roman0104 <roman0104@rambler.ru>
2022-05-31 20:12:10 +0300

    Update parse_tululu_category.py

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -18,27 +18,33 @@ def main() -> None:
     logger.addHandler(file_handler)
 
     url = "https://tululu.org/"
-    book_category = "l55"
-    link_book_category = urljoin(url, book_category)
+    book_category = "/l55/"
+    link_category = urljoin(url, book_category)
 
-    try:
-        response = requests.get(link_book_category)
-        response.raise_for_status()
-    except requests.exceptions.ConnectionError:
-        sys.stderr.write(f"Ошибка соединения\n")
-        logger.error(f"Ошибка соединения")
-        time.sleep(3)
-        return
-    except requests.exceptions.HTTPError:
-        sys.stderr.write(f"Ошибка на странице\n")
-        logger.error(f"Ошибка на странице")
-        return
+    for page in range(1, 11):
+        link_category_page = urljoin(link_category, str(page))
 
-    soup = BeautifulSoup(response.text, "lxml")
-    parse_content_table = soup.find(id="content").find_all("table")
+        try:
+            response = requests.get(link_category_page)
+            response.raise_for_status()
+        except requests.exceptions.ConnectionError:
+            sys.stderr.write(f"Ошибка соединения\n")
+            logger.error(f"Ошибка соединения")
+            time.sleep(3)
+            return
+        except requests.exceptions.HTTPError:
+            sys.stderr.write(f"Ошибка на странице\n")
+            logger.error(f"Ошибка на странице")
+            return
 
-    for book in parse_content_table:
-        print(urljoin(url, book.find("a")["href"]))
+        soup = BeautifulSoup(response.text, "lxml")
+        parse_content_table = soup.find(id="content").find_all("table")
+
+        for book in parse_content_table:
+            print(urljoin(url, book.find("a")["href"]))
+            logger.info(f'Страница {page} ссылка: {urljoin(url, book.find("a")["href"])}')
+
+        logger.info(f"Парсер успешно распарсил страницу {page}")
 
     logger.info(f"Парсер успешно завершил работу")
 
